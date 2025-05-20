17ef0bfc419a55fb81fa032095c62368b28f026d
geetu040 <raoarmaghanshakir040@gmail.com>
2025-05-20 13:24:08 +0500

    fix answer parsing for llama

--- chatbot.py
+++ chatbot.py
@@ -230,7 +230,7 @@ class Chatbot:
 			if "think" in answer:
 				answer = answer[answer.index("</think>")+8:].strip()
 			elif "assistant\n" in answer:
-				answer = answer[answer.rfind("assistant")+8:].strip()
+				answer = answer[answer.rfind("assistant")+9:].strip()
 			answers.append(answer)
 
 		return answers
